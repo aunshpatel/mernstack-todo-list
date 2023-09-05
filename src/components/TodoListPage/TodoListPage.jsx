93057c76a5ea826ed43c584be0b234e60e5293f4
aunshpatel <patelaunsh28@gmail.com>
2023-09-05 16:29:06 -0400

    Code cleanup

--- src/components/TodoListPage/TodoListPage.jsx
+++ src/components/TodoListPage/TodoListPage.jsx
@@ -2,41 +2,41 @@ import { useState, useEffect } from 'react';
 import * as todosAPI from "../../utilities/todos-api";
 
 export default function TodoListPage() {
-    const [todos, setTodos] = useState([]);
-    
-    useEffect(() => {
-      async function getNotes() {
-        const todos = await todosAPI.getAll();
-        // const sortedNotes = sortNotes(notes, sortOrder);
-        setTodos(todos);
-      }
-      getNotes();
-    });
+  const [todos, setTodos] = useState([]);
+  
+  useEffect(() => {
+    async function getNotes() {
+      const todos = await todosAPI.getAll();
+      // const sortedNotes = sortNotes(notes, sortOrder);
+      setTodos(todos);
+    }
+    getNotes();
+  });
 
-    return (
-      <>
-        <h1>Todo List</h1>
-        {/* <NewNoteForm handleAddNote={handleAddNote} /> */}
-        {todos.length === 0 ? (
-          <p>No Todo Added Yet!</p>
-        ) : (
-          <table>
+  return (
+    <>
+      <h1>Todo List</h1>
+      {/* <NewNoteForm handleAddNote={handleAddNote} /> */}
+      {todos.length === 0 ? (
+        <p>No Todo Added Yet!</p>
+      ) : (
+        <table>
+          <tr>
+            <th>Task</th>
+            <th>Status</th>
+            <th>Start Date</th>
+            <th>End Date</th>
+          </tr>
+          {todos.map((todo)=>(
             <tr>
-                <th>Task</th>
-                <th>Status</th>
-                <th>Start Date</th>
-                <th>End Date</th>
+              <td>{todo.taskText}</td>
+              <td>{todo.taskStatus}</td>
+              <td>{todo.startDate}</td>
+              <td>{todo.endDate}</td>
             </tr>
-            {todos.map((todo)=>(
-                <tr>
-                    <td>{todo.taskText}</td>
-                    <td>{todo.taskStatus}</td>
-                    <td>{todo.startDate}</td>
-                    <td>{todo.endDate}</td>
-                </tr>
-            ))}
-          </table>
-        )}
-      </>
-    );
-  }
+          ))}
+        </table>
+      )}
+    </>
+  );
+}
