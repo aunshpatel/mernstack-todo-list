c50a35acef0c90ec05ac30f648882dc9ebc88e7d
aunshpatel <patelaunsh28@gmail.com>
2023-09-06 11:51:07 -0400

    Update Todo List Table Design

--- src/components/TodoListPage/TodoListPage.jsx
+++ src/components/TodoListPage/TodoListPage.jsx
@@ -1,8 +1,40 @@
 import { useState, useEffect } from 'react';
 import * as todosAPI from "../../utilities/todos-api";
+import DataTable from 'react-data-table-component';
+import '../../index.css'
 
 export default function TodoListPage() {
   const [todos, setTodos] = useState([]);
+  const columns = [
+    {
+        name: '#',
+        selector: row => row.index,
+        sortable: true,
+    },
+    {
+        name: 'Task',
+        selector: row => row.task,
+        sortable: true,
+    },{
+      name: 'Status',
+      selector: row => row.status,
+      sortable: true,
+    },{
+      name: 'Start Date',
+      selector: row => row.startdate,
+      sortable: true,
+    },{
+      name: 'End Date',
+      selector: row => row.enddate,
+      sortable: true,
+    },
+  ];
+
+  const paginationComponentOptions = {
+    rowsPerPageText: 'Rows Per Page',
+    rangeSeparatorText: 'of',
+    selectAllRowsItem: true,
+  };
   
   useEffect(() => {
     async function getNotes() {
@@ -16,27 +48,31 @@ export default function TodoListPage() {
   return (
     <>
       <h1>Todo List</h1>
-      {/* <NewNoteForm handleAddNote={handleAddNote} /> */}
       {todos.length === 0 ? (
         <p>No Todo Added Yet!</p>
       ) : (
-        <table>
-          <tr>
-            <th>Task</th>
-            <th>Status</th>
-            <th>Start Date</th>
-            <th>End Date</th>
-          </tr>
-          {todos.map((todo)=>(
-            <tr>
-              <td>{todo.taskText}</td>
-              <td>{todo.taskStatus}</td>
-              <td>{todo.startDate}</td>
-              <td>{todo.endDate}</td>
-            </tr>
-          ))}
-        </table>
+       <div className="todoTableDiv">
+         <div>
+          <DataTable
+            columns={columns}
+            data={
+              todos.map((todo, idx)=>(
+                {
+                  index: idx+1,
+                  task: todo.taskText,
+                  status: todo.taskStatus,
+                  startdate:todo.startDate,
+                  enddate:todo.endDate,
+              }
+              ))
+            }
+            pagination
+            paginationComponentOptions={paginationComponentOptions}
+        />
+        </div>
+       </div>
       )}
+      
     </>
   );
 }
