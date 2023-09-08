877b909a4e709cf90956bd560eea9bc2840d1eb0
aunshpatel <patelaunsh28@gmail.com>
2023-09-08 16:35:33 -0400

    Add title to myprofile

--- src/components/MyProfilePage/MyProfilePage.jsx
+++ src/components/MyProfilePage/MyProfilePage.jsx
@@ -2,6 +2,8 @@ export default function ProfilePage({ user, setUser }) {
   return (
     <div>
       <div class="new-note-form">
+        <h2>My Profile:</h2>
+        <br />
         <div class="formDiv">
           <form className="input-box">
               <div class="row mb-4">
